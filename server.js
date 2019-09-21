b7e63ada195d8da209ecb532ecab5dce3ddfffee
shayanxm <shayan.mwp@gmail.com>
2019-09-21 11:43:23 +0430

    add application

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'), 
+  Task = require('./api/models/factorModel'),
+  Task = require('./api/models/productModel'),
+  Task=require('./api/models/docModel')//created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+
+app.listen(port);
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+console.log('todo list RESTful API server started on: ' + port);
